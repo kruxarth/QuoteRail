f8b6602b5699f18b53d655a58c45912db0c6070e
kruxarth <kruxarth@gmail.com>
2026-09-01 21:02:03 +0530

    deployment issue

--- tests/setup.ts
+++ tests/setup.ts
@@ -6,7 +6,6 @@ config({ path: '.env' });
 beforeAll(() => {
   process.env.FAKE_AI = 'true';
   process.env.FAKE_PAYMENTS = 'true';
-  process.env.NODE_ENV = 'test';
   process.env.REAL_RAZORPAY_TESTS_ENABLED = 'false';
   process.env.DATABASE_URL =
     process.env.LOCAL_DATABASE_URL ?? 'postgres://quoterail:quoterail@localhost:5432/quoterail';
